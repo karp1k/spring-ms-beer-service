5db2d9ba6469c610bbfc8096f4f7ccce081a2949
Tom <desktop-vm-ubuntu>
2020-04-15 15:15:37 +0300

    Closes #1 Add initial project

---
+++ src/main/java/guru/springframework/springmsbeerservice/SpringMsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springmsbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springmsbeerservice/SpringMsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springmsbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
