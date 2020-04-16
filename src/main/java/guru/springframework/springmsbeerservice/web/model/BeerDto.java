49c71c7379f51895b44272ca33969d27be8b81e0
Tom <desktop-vm-ubuntu>
2020-04-16 11:36:01 +0300

    Closes #8 BeerDto validation annotations

--- src/main/java/guru/springframework/springmsbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/springmsbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -18,13 +22,23 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
-    private UUID id;
-    private Integer version;
-    private OffsetDateTime createdDate;
-    private OffsetDateTime lastModifiedDate;
+    @Null
+    private UUID id; // read-only properties for client
+    @Null
+    private Integer version; // read-only properties for client
+    @Null
+    private OffsetDateTime createdDate; // read-only properties for client
+    @Null
+    private OffsetDateTime lastModifiedDate; // read-only properties for client
+    @NotBlank
     private String name;
+    @NotNull
     private BeerStyleEnum beerStyle;
+    @Positive
+    @NotNull
     private Long upc;
+    @Positive
+    @NotNull
     private BigDecimal price;
     private Integer quantityOnHand;
 
