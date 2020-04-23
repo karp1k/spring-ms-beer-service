4638650ab2774fe88a46dc5ad901001b72f4854f
Tom <desktop-vm-ubuntu>
2020-04-23 13:28:19 +0300

    Closes #15 Impl BeerService

--- src/main/java/guru/springframework/springmsbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/springmsbeerservice/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package guru.springframework.springmsbeerservice.web.controller;
 
+import guru.springframework.springmsbeerservice.Services.BeerService;
 import guru.springframework.springmsbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,22 +14,28 @@ import java.util.UUID;
 /**
  * @author kas
  */
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
+    private final BeerService beerService;
+
     @GetMapping("/{id}")
-    public ResponseEntity getBeerById(@PathVariable UUID id) {
-        return new ResponseEntity(BeerDto.builder().build(), HttpStatus.OK);
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID id) {
+        BeerDto dto = beerService.getById(id);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity saveBeer(@Validated @RequestBody BeerDto dto) {
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity<BeerDto> saveBeer(@Validated @RequestBody BeerDto dto) {
+        BeerDto saved = beerService.save(dto);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity updateBeer(@PathVariable UUID id, @Validated @RequestBody BeerDto dto) {
+        beerService.update(id, dto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
