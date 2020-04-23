600b4c1995bb65d733235853f5c40b1a81a0b1f5
Tom <desktop-vm-ubuntu>
2020-04-23 13:04:01 +0300

    Jackson and JSON Add Jackson Annotation to BeerDto Add custom serializer/deserializer for LocalDate (for fun) Closes #13

--- src/main/java/guru/springframework/springmsbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/springmsbeerservice/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package guru.springframework.springmsbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +14,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -21,25 +26,41 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDto {
-
+    // Override PropertyNamingStrategy
+    // @JsonProperty("beerId") rename property from id to beerId in JSON
     @Null
     private UUID id; // read-only properties for client
     @Null
     private Integer version; // read-only properties for client
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate; // read-only properties for client
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate; // read-only properties for client
     @NotBlank
     private String name;
+
     @NotNull
     private BeerStyleEnum beerStyle;
+
     @Positive
     @NotNull
     private Long upc;
+
+    // BigDecimal to String
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
     private Integer quantityOnHand;
+
+    // Just for fun with Custom serializer and deserializer
+    //@JsonDeserialize(using = LocalDateDeserializer.class)
+    // From yyyy-mm-dd to yyyymmdd
+    //@JsonSerialize(using = LocalDateSerializer.class)
+    //private LocalDate localDate;
 
 }
---
+++ src/test/java/guru/springframework/springmsbeerservice/web/model/BeerDtoJsonTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.springmsbeerservice.web.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+// Profile for SNAKE_CASE of JSON properties names
+@ActiveProfiles("snake")
+@JsonTest
+class BeerDtoJsonTest {
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    public void serializeDto() throws JsonProcessingException {
+        BeerDto dto = getDto();
+        String jsonString = mapper.writeValueAsString(dto);
+        System.out.println(jsonString);
+
+    }
+
+    @Test
+    void deserializeString() throws JsonProcessingException {
+        String jsonString = "{\"id\":\"e03a21c9-622c-414a-bb7d-084c6cc74a62\",\"version\":null,\"created_date\":\"2020-04-23T12:58:47+0300\",\"last_modified_date\":\"2020-04-23T12:58:47+0300\",\"name\":\"Name\",\"beer_style\":\"ALE\",\"upc\":1,\"price\":\"10\",\"quantity_on_hand\":null}";
+        BeerDto dto = mapper.readValue(jsonString, BeerDto.class);
+        System.out.println(dto);
+    }
+
+    BeerDto getDto() {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Name")
+                .beerStyle(BeerStyleEnum.ALE)
+                .createdDate(OffsetDateTime.now())
+                .lastModifiedDate(OffsetDateTime.now())
+                .upc(1L)
+                .price(BigDecimal.TEN)
+                //.localDate(LocalDate.now())
+                .build();
+    }
+
+
+}
